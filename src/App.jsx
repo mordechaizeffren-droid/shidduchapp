4901b6772978317ce6bdf480dba505ccfff1ede5
mordechaizeffren-droid <mordechaizeffren@gmail.com>
2025-09-02 12:40:28 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>It works 🎉</h1>
+      <p>If you can see this, React is mounting correctly.</p>
+    </div>
+  );
+}
